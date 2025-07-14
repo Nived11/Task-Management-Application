6b4af09a6df38c12a1941d1c1d21513dde217ae3
Nived <nivedsuresh5@gmail.com>
2025-07-14 13:12:58 +0530

    backend completed..

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/api", router); 
 
+app.get("/", (req, res) => {
+  res.send("Listify backend is alive!");
+});
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, async () => {
